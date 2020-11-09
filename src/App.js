9e1043ea06569503746fb397bdc0968e6ecb9a52
ヌルポ <Le.Cai-1@student.uts.edu.au>
2020-11-09 14:55:16 +1100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
-
+import './css/os.css';
+import folder from './img/icons/folder.png'
+import termial from './img/icons/termial.png'
+import music from './img/icons/music.png'
+import pc from './img/icons/pc.png'
+import internet from './img/icons/internet.png'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App hex-os">
+
+      <div className="hex-menu-top">
+        <div className="hex-menu-top-content">
+          <div className="hex-menu-item">
+            <li><b>HEX OS</b></li>
+            <li><a href="#">アプリケーション</a></li>
+            <li><a href="#">情報</a></li>
+          </div>
+          <div className="hex-menu-item item-right">
+            <li>言語</li>
+            <li>Sparrow</li>
+            
+          </div>
+        </div>
+      </div>
+      <div className="hex-menu-bottom">
+        <div className="hex-menu-bottom-content">
+          <div className="hex-window-list">
+            <li>ブラウザ</li>
+            <li className="active">ABOUT HEX</li>
+          </div>
+          <div className="hex-menu-item item-right">
+            <li>TIME: 03:00 AM</li>
+          </div>
+        </div>
+      </div>
+      <div className="hex-gird">
+        <li>
+          <img src={pc} alt=""/>
+          <p>This PC</p>
+        </li>
+        <li>
+          <img src={internet} alt=""/>
+          <p>Blog</p>
+        </li>
+        <li>
+          <img src={music} alt=""/>
+          <p>Music</p>
+        </li>
+        <li>
+          <img src={folder} alt=""/>
+          <p>MIDI</p>
+        </li>
+        <li>
+          <img src={termial} alt=""/>
+          <p>Termial</p>
+        </li>
+      </div>
     </div>
   );
 }
